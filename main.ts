1c20ab426d1b3e640adf5954bdcbc4c78103583e
John Grinwis <john@grinwis.com>
2024-04-12 07:40:49 +0200

    added some extra comments

--- main.ts
+++ main.ts
@@ -1,25 +1,27 @@
 /*
 (c) Copyright 2024 Akamai Technologies, Inc. Licensed under Apache 2 license.
 
-Version: 0.0.14
-Purpose: if the defined bidblocks is exactly the same as AF_FIREWALL bid rules, generate a response.
-This version there should be an exact match but feel free to modify the check
+Version: 0.0.15
+Purpose: if the defined staticRuleIds is exactly the same as AF_FIREWALL bid rules, generate a response.
+This version checking for Browser Impersonator Detection (BID) rules. If there is a match, respond with the bidMatchResponse
 */
 
-// the exact list of BIDS you want to block on.
+// the exact list of BIDs you want to block on.
 const staticRuleIds: string = "3904000:3904003:3904006:3904042";
 
-// the http response when there is a BID match
+// the HTTP response when there is a BID match
 const bidMatchResponse = {
   status: 403,
   headers: { "Content-Type": "application/json;charset=utf-8" },
   body: JSON.stringify({ error: staticRuleIds }),
-  statusText: "Denied Response",
+  deny_reason: "Denied Response",
 };
 
-// unpack our response so it can be feed into responseWith()
-// https://techdocs.akamai.com/edgeworkers/docs/request-object#respondwith
-const { status, headers, body, statusText } = bidMatchResponse;
+/*
+unpack our response so it can be used in responseWith() call
+https://techdocs.akamai.com/edgeworkers/docs/request-object#respondwith
+*/
+const { status, headers, body, deny_reason } = bidMatchResponse;
 
 /**
  * Check if all elements in array1 exists in array2.
@@ -41,7 +43,7 @@ const isEqual = (array1: string[], array2: string[]) =>
 
 /**
  * Creates a sorted list of Browser Impersonator Detection (BID) rules starting with '3904'.
- * It expect a list of firewall rules like from a string of firewall rules like 3904000:3904003:3904006:3904042:BOT-BROWSER-IMPERSONATOR
+ * It expects a list of detected firewall rules like 3904000:3904003:3904006:3904042:BOT-BROWSER-IMPERSONATOR
  * @param bids A string containing Akamai triggered firewall rules, separated by a colon (':').
  * @returns An array of only the BID rules, starting with '3904', sorted alphabetically.
  */
@@ -64,13 +66,13 @@ export async function onClientRequest(request: EW.IngressOriginRequest) {
   const detectedRuleIds: string = request.getVariable("PMUSER_FW_RULES");
 
   /* 
-  Only take action if the FW_RULES is set. 
+  Only take action when the FW_RULES is set. 
   This should be done in delivery config not to waste time and EW calls.
   */
   if (detectedRuleIds !== null && detectedRuleIds.length > 0) {
     // this version detected and pre-defined list should be exactly the same
     if (isEqual(createBidList(detectedRuleIds), createBidList(staticRuleIds))) {
-      request.respondWith(status, headers, body, statusText);
+      request.respondWith(status, headers, body, deny_reason);
     }
   }
 }
